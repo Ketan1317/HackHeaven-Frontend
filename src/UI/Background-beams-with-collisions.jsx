28dc33cb5809a4845573f91f014dc3ecec12bf6d
Ketan Goyal <goyalketan1317@gmail.com>
2025-04-11 11:53:47 +0530

    deployinggg

--- src/UI/Background-beams-with-collisions.jsx
+++ src/UI/Background-beams-with-collisions.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { cn } from "@/lib/utils";
-import { AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "motion/react";
 import React, { useRef, useState, useEffect } from "react";
 
 export const BackgroundBeamsWithCollision = ({
